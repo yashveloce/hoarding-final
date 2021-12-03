11c153442226c0f7fafaa277d5305c7651bcf0a8
yashveloce <yash.s@velocetechinsights.com>
2021-12-03 11:10:35 +0530

    Tenth Commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -39,7 +39,7 @@ const Login=() =>
                 auth.login(()=>{
                     localStorage.setItem("userrole",user.role)
                     localStorage.setItem("userdata",formState.username);
-                    history.push("/Login")
+                    history.push("/")
                 })
                 //console.log("-----------------"+localStorage.getItem("authenticated"));
             }
